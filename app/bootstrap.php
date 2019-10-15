ff2b10fb6842c3c06f17c1b8173d164851a690c4
Masaki Kawabata Neto <kawabata@usp.br>
2019-10-15 16:40:30 -0300

    Limpeza e novo método forbidden

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 // vamos ajustar os caminhos baseados no arquivo de configuracao
-// dessa forma não precisamos recorrer ao RewriteBase do apache 
+// dessa forma não precisamos recorrer ao RewriteBase do apache
 // e podemos usar um .htaccess que não depende do deploy
 Flight::request()->base = parse_url(DOMINIO, PHP_URL_PATH);
 Flight::request()->url = str_replace(Flight::request()->base, '', Flight::request()->url);
@@ -28,11 +28,19 @@ Flight::map('requireAuth', function () {
 });
 
 // vamos sobrescrever a mensagem de not found para ficar mais compatível com a API
-Flight::map('notFound', function () {
-    // mostrar dados do request
-    // $data['request']=Flight::request();
-
-    $data['message'] = 'Not Found';
+// retorna 404 mas com mensagem personalizada opcional ou mensagem padrão
+Flight::map('notFound', function ($msg = null) {
+    $data['message'] = empty($msg) ? 'Not Found' : $msg;
     $data['documentation_url'] = DOMINIO . '/';
-    Flight::jsonf($data);
+    $json = json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+    Flight::halt(404, $json);
+});
+
+// Interrompe a execução com 403 - Forbidden
+// usado quando negado acesso por IP
+Flight::map('forbidden', function ($msg = null) {
+    $data['message'] = empty($msg) ? 'Forbidden' : $msg;
+    $data['documentation_url'] = DOMINIO . '/';
+    $json = json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+    Flight::halt(403, $json);
 });
