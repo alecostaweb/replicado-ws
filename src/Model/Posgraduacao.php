cbcabe0a642062627cc6b6cfe42a8550060e1465
Masaki Kawabata Neto <kawabata@usp.br>
2021-07-08 12:59:10 -0300

    revertendo tratamento especial 1o sem 2021

--- src/Model/Posgraduacao.php
+++ src/Model/Posgraduacao.php
@@ -29,17 +29,7 @@ class Posgraduacao
     // vamos pegar todos os dados aqui recursivamente
     public static function disciplinasOferecimento($codare)
     {
-        // com a pandemia as disciplinas ficaram atrasadas então vamos passar as datas de inicio e fim
-        // para o método pois o calculo automático dá errado.
-        // Tratando o 1o semestre de 2021
-        $today = new \DateTime('now');
-        $ini = new \DateTime('1-1-2021');
-        $fim = new \DateTime('30-7-2021');
-        if ($today >= $ini && $today <= $fim) {
-            $disciplinas = UspdevPosgraduacao::disciplinasOferecimento($codare, '20210101', '20210730');
-        } else {
-            $disciplinas = UspdevPosgraduacao::disciplinasOferecimento($codare);
-        }
+        $disciplinas = UspdevPosgraduacao::disciplinasOferecimento($codare);
 
         for ($i = 0; $i < count($disciplinas); $i++) {
             #vamos remover timestamps que dá problema em json
